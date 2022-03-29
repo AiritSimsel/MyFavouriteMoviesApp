d7cd912d9ae595965c01c93f925b5b4e24cb594d
Airit Simsel <airitkarm@gmail.com>
2022-03-29 20:45:58 +0300

    missing movies.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,28 @@ import React from "react";
 import PageHeading from "./PageHeading";
 import List from "./List";
 import Footer from "./Footer";
+import movies from "../movies";
 
 
 export default function App() {
+
+  function createList(movie) {
+    return (
+      <List 
+        key={movie.id}
+        name={movie.name}
+        image={movie.image}
+        alt={movie.alt}
+      />
+    )
+  }
     
     
   return (
-    <div>
+    <>
       <PageHeading title="My favourite movies" />
-      <List name="Interstellar" image="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg" alt="Interstellar" />
-      <List name="Wall-E" image="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hbhFnRzzg6ZDmm8YAmxBnQpQIPh.jpg" alt="Wall-E" />
-      <List name="Little Shop of Horrors (1960)" image="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8wukZauAXzmtG5uIsJwk4TO1yvo.jpg" alt="LSoH" />
+      {movies.map(createList)}
       <Footer />
-    </div>
+    </>
   );
 }
---
+++ src/movies.js
@@ -0,0 +1,28 @@
+let movies = [
+    {
+        id: "1",
+        name: "Interstellar",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+        alt: "Interstellar"
+    },
+    {
+        id: "2",
+        name: "Wall-E",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hbhFnRzzg6ZDmm8YAmxBnQpQIPh.jpg",
+        alt: "Wall-E"
+    },
+    {
+        id: "3",
+        name: "Little Shop of Horrors (1960)",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8wukZauAXzmtG5uIsJwk4TO1yvo.jpg",
+        alt: "Little Shop of Horrors"
+    },
+    {
+        id: "4",
+        name: "Tangerines",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jNKUXcwZr1vpgq7sGTCEqdngo4Z.jpg",
+        alt: "Tangerines"
+    }
+]
+
+export default movies;
